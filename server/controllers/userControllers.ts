eaf82f4b2ca0a41cc14532eba66ca7b7199dcc52
LuKz <lucaslopeslukz@gmail.com>
2026-05-21 04:14:52 -0400

    feat: add login functionality with JWT authentication and user verification

--- server/controllers/userControllers.ts
+++ server/controllers/userControllers.ts
@@ -2,6 +2,8 @@ import { Request, Response, NextFunction } from 'express'
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt'
 import { supabase } from '../db/connection'
+import dotenv from 'dotenv'
+dotenv.config()
 
 export const getUser = async (
   req: Request,
@@ -50,6 +52,48 @@ export const signup = async (
     res.status(201).json({
       message: 'Signup successful. Please verify your email.',
       user: profile,
+    })
+  } catch (err) {
+    const message = err instanceof Error ? err.message : 'Internal server error'
+    res.status(500).json({ message })
+  }
+}
+
+export const login = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const { email, password } = req.body
+
+    // 2️⃣ Fetch user profile
+    const { data: profile, error: profileError } = await supabase
+      .from('users')
+      .select('*')
+      .eq('email', email)
+      .single()
+
+    if (profileError || !profile) {
+      return res.status(401).json({ message: 'Invalid credentials' })
+    }
+    const isValid = await bcrypt.compare(password, profile.password_hash)
+
+    if (!isValid) {
+      return res.status(401).json({ message: 'Invalid credentials' })
+    }
+
+    const token = jwt.sign(
+      { id: profile.id, email: profile.email },
+      process.env.JWT_SECRET as string,
+      { expiresIn: '99999999d' },
+    )
+
+    res.json({
+      token,
+      user: {
+        profile,
+      },
     })
   } catch (err) {
     const message = err instanceof Error ? err.message : 'Internal server error'
