9c0b2058e9dc296d6e356cdc5b959b0fcb35de5c
LuKz <lucaslopeslukz@gmail.com>
2026-05-24 13:51:55 -0400

    feat: update server configuration and enhance challenge randomizer utility

--- server/utils/challenge_randomizer.ts
+++ server/utils/challenge_randomizer.ts
@@ -1,20 +1,38 @@
-export const challenge_randomizer = (variables_range, alternative_options) => {
+const Parser = require('expr-eval').Parser
+
+export const challenge_randomizer = (variables_range, alternatives_options) => {
+  const parser = new Parser()
   const variables = []
   for (let i = 0; i < variables_range.length; i++) {
-    variables.push(Math.floor(Math.random() * variables_range[i]))
-    console.log(variables)
+    variables.push(Math.floor(Math.random() * variables_range[i] + 1))
   }
 
+  const resolved_alternatives: string[] = []
+  for (let i = 0; i < alternatives_options.length; i++) {
+    const resolved = alternatives_options[i].replace(
+      /\{(\d+)\}/g,
+      (_, i) => variables[Number(i)] ?? `{${i}}`,
+    )
+    resolved_alternatives.push(resolved)
+  }
+
+  const evaluated_alternatives = resolved_alternatives.map((expr) =>
+    parser.evaluate(expr)
+  )
+
   const alternatives = []
-  const alternatives_static = alternative_options
-  let alt = alternatives_static
+  let alt = [...evaluated_alternatives]
   for (let i = 0; i < 4; i++) {
     let choosen = Math.floor(Math.random() * alt.length)
 
     if (!Number.isInteger(alt[choosen])) {
       const numerator = variables[0]
       const denominator = variables[1]
-      alt[choosen] = `${numerator}/${denominator}`
+      if ((numerator / denominator) % 0) {
+        alt[choosen] = numerator / denominator
+      } else {
+        alt[choosen] = `${numerator}/${denominator}`
+      }
     }
     alternatives.push(alt[choosen])
 
