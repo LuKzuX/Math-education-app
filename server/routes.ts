35e9263c71a1f20410003e115cf661d25a4ce601
LuKz <lucaslopeslukz@gmail.com>
2026-06-05 21:58:37 -0400

    feat: refactor controllers and routes for better organization and clarity

--- server/routes.ts
+++ server/routes.ts
@@ -2,7 +2,9 @@ import { Router } from "express";
 export const router = Router()
 import { userAuth } from "./middlewares/userAuth";
 import { signup, signin, verifyEmail, forgotPassword, resetPassword, getUser, getUserAttempts, updateUser } from "./controllers/userControllers";
-import { createPath, createTopic, getPaths, getTopics, getTopicsByPath, getChallengesByTopic, getChallenges, getChallenge, submitAnswer, createChallenge } from './controllers/pathsAndSiblingsControllers'
+import { getPaths, createPath } from './controllers/pathsControllers'
+import { getTopics, createTopic } from './controllers/topicsControllers'
+import { getChallenges, getChallenge, createChallenge, submitAnswer } from './controllers/challengesControllers'
 import { getAchievements, getUserAchievements } from "./controllers/achievments";
 
 router.get('/user', userAuth, getUser)
@@ -15,15 +17,21 @@ router.get('/user-attempts', userAuth, getUserAttempts)
 router.patch('/user/update', userAuth, updateUser)
 
 
+// Paths
 router.get('/paths', getPaths)
-router.get('/paths/:path_id/topics', getTopics)
-router.get('/topics/:topic_id/challenges', getChallenges)
-router.post('/new-path', createPath)
-router.post('/new-topic/:path_id', createTopic)
-router.post('/new-challenge/:topic_id', createChallenge)
-router.get('/challenge/:challenge_id', getChallenge)
-router.post('/challenge/:challenge_id', userAuth, submitAnswer)
+router.post('/paths', createPath)
 
+// Topics
+router.get('/paths/:pathId/topics', getTopics)
+router.post('/paths/:pathId/topics', createTopic)
+
+// Challenges
+router.get('/topics/:topicId/challenges', getChallenges)
+router.post('/topics/:topicId/challenges', createChallenge)
+
+// Challenge
+router.get('/challenges/:challengeId', getChallenge)
+router.post('/challenges/:challengeId/submit', userAuth, submitAnswer)
 
 router.get('/achievements', getAchievements)
 router.get('/user/achievements', userAuth, getUserAchievements)
