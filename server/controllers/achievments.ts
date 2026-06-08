9a383b452a6b5d0d2f48c1d46e80e3fcc222a669
Lucas Lopes <lucaslopeslukz@gmail.com>
2026-06-07 23:00:31 -0400

    implemented multer abd file upload

--- server/controllers/achievments.ts
+++ server/controllers/achievments.ts
@@ -1,6 +1,7 @@
 import { RequestHandler } from 'express'
 import { supabase } from '../db/connection'
 import { AuthRequest } from '../types/AuthRequest'
+import { upload } from '../middlewares/multer'
 
 export const getAchievements: RequestHandler = async (req, res, next) => {
   const { data, error } = await supabase
@@ -23,9 +24,27 @@ export const getUserAchievements: RequestHandler = async (req: AuthRequest, res,
   return res.status(200).json(data)
 }
 
+
+
 export const createAchievement: RequestHandler = async (req: AuthRequest, res, next) => {
   if (!req.user) return res.send("invalid user logged")
   const user_id = req.user.id
- 
+  const { title, description, xp_earned } = req.body
+  const file = req.file
+  const fileName = file?.originalname
+  if (!file || !fileName) return res.status(400).json({ error: 'icon is required' })
+  const { data, error } = await supabase.storage.from('achievements_icons')
+    .upload(fileName, file.buffer, { contentType: file.mimetype })
+  const { data: { publicUrl } } = supabase.storage
+    .from('achievements_icons')
+    .getPublicUrl(fileName)
+
+  await supabase.from('achievements').insert({
+    title,
+    description,
+    xp_earned,
+    icon_url: publicUrl
+  })
+
 }
 
