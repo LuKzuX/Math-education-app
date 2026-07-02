11483e58d5cc88bf8d433919330920e3b035093b
Lucas <lucaslopeslukz@gmail.com>
2026-07-02 16:33:02 -0400

    a

--- client/src/pages/Achievements.tsx
+++ client/src/pages/Achievements.tsx
@@ -1,4 +1,20 @@
+import { useEffect } from "react"
+import axios from "axios"
+
 function Achievements() {
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const data = await axios.get("/mathly/achievements")
+        console.log(data.data);
+
+      } catch (error) {
+        console.log(error);
+
+      }
+    }
+    getData()
+  }, [])
   return (
     <div>
       <h1>Achievements</h1>
